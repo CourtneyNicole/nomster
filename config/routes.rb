171fb7dfc13a842edfc94c542e32c9443d279b7d
CourtneyNicole <courtney_nicole_flynn@yahoo.com>
2019-06-12 01:33:52 +0000

    user profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    end
+   resources :users, only: :show
 end
